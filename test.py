8438ce74d329c05678a58b2800824abeb38e8b2c
ashrondb@gmail.com <kaa@cniitei.org>
2017-06-13 15:17:33 +0300

    v1.01
    Удаленные файлы выделены в отдельный "проект"

--- test.py
+++ test.py
@@ -1,45 +1,20 @@
-# -*- coding: utf-8 -*-
 """
-Created on Thu Jun  8 13:33:40 2017
+Created on Tue Jun 13 10:30:55 2017
 
 @author: KAA
 """
 
-share = "MTLRP"
-info_share ='moex-classica/'+share.lower() 
-        
-from urllib.request import urlopen
-finam_symbols = urlopen('http://www.finam.ru/cache/icharts/icharts.js').readlines()
+from sharehelper import ShareHelper
 
-#print(finam_symbols[8])
+sh = ShareHelper()
+#m =sh.get_code('mosenrg')
+l =sh.get_code('tmk-ao')
+#print(len(l))
+#print(l[0][0])
+#print(l[0][1])
+#
+#print(l[1][0])
 
-moexakcii = finam_symbols[8].decode("utf-8")
-print(moexakcii)
-
-#print(len('": "moex-akcii/'))
-#print(len('","'))
-#print(len('": "moex-akcii/'))
-
-s = 0
-start = '": "moex-akcii/'
-s_len = len(start)
-m_1 = 0
-m_2 = 0
-midle = '","'
-m_len = len(midle)
-e = 0
-end = '": "'
-e_len = len(end)
-
-n = 0
-print(moexakcii.count(start))
-while n < moexakcii.count(start) :
-    s = moexakcii.find(start,s,len(moexakcii))+s_len
-    m_1 = moexakcii.find(midle,s,len(moexakcii))
-    m_2 = m_1 + m_len
-    e = moexakcii.find(end,m_2,len(moexakcii))
-    print(moexakcii[s:m_1],' : ',moexakcii[m_2:e])
-    n = n+1
-        
-
-    
+list_code = sh.get_listcode()
+print(list_code[0][0])
+print(list_code[1][0])
--- tester2.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jun  9 12:43:51 2017
-
-@author: KAA
-"""
-
-s = 'asdfj;waiejfa;ksdjfawilefj;smv;aiefjz;sxz,cvwrs'
-print(s.count('a'))
